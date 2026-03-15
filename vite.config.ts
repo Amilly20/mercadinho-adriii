4fecd4b0d3ba22eb672c616a0921bade84a98fa1
Amlly20 <amillynolasco@gmail.com>
2026-03-15 19:01:04 -0300

    fix: restaura a configuração do atalho @ no vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 export default defineConfig({
   base: "/mercadinho-adriii/", // <-- ADICIONE ESTA LINHA COM O NOME DO SEU REPOSITÓRIO
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
-
-
